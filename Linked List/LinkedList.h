462681198fb6a502e410d8c51e2a04eb172f4ef9
xeaza <taylorwrightsanson@gmail.com>
2014-12-15 11:39:06 -0500

    Created Linked List class

---
+++ Linked List/LinkedList.h
@@ -0,0 +1,13 @@
+//
+//  LinkedList.h
+//  Linked List
+//
+//  Created by Taylor Wright-Sanson on 12/15/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LinkedList : NSObject
+
+@end
---
+++ Linked List/LinkedList.m
@@ -0,0 +1,13 @@
+//
+//  LinkedList.m
+//  Linked List
+//
+//  Created by Taylor Wright-Sanson on 12/15/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "LinkedList.h"
+
+@implementation LinkedList
+
+@end
